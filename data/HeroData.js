bb9f71f179eb1c2179f96f41f320f090f55cd317
Maksym Sagadin <maxsagadin@gmail.com>
2022-07-11 11:03:49 -0700

    updated hero title

--- data/HeroData.js
+++ data/HeroData.js
@@ -1,6 +1,6 @@
 export const heroData = {
     heading: "Hi, I'm ",
     name: 'Maksym',
-    title: 'A Front-End Web Developer',
+    title: 'A Web Developer',
     statement: 'I specialize in building responsive front-end web applications that provide an exceptional experience.',
 }
