213b7a85b1d830018dbe262bb3e7087eaac73c8b
Maksym Sagadin <51523851+maksymsagadin@users.noreply.github.com>
2022-09-22 17:38:24 -0700

    Update HeroData.js

--- data/HeroData.js
+++ data/HeroData.js
@@ -1,6 +1,5 @@
 export const heroData = {
     heading: "Hello, I'm ",
     name: 'Maksym Sagadin.',
-    title: '',
-    statement: 'A Web Developer specializing in in Front End with React.js.',
+    statement: 'Frontend developer specializing in React.js websites and applications.',
 }
