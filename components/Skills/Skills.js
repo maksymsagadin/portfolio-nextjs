375560c481d487e91e2e0723ea0a73d31582b18a
Maksym Sagadin <maxsagadin@gmail.com>
2022-10-03 14:12:34 -0700

    added Tilt feature

--- components/Skills/Skills.js
+++ components/Skills/Skills.js
@@ -3,20 +3,25 @@ import Image from 'next/image'
 import { Section, Container, Heading } from '../../styles/globalStyles'
 import { SkillsGrid, SkillWrapper, SkillText } from './SkillsStyles'
 import { skillData } from '../../data/SkillData'
-import bgimg from '/public/backgrounds/bg-creme.png'
+import bgimg from '/public/backgrounds/bg-blue.jpg'
+import Tilt from 'react-tilt'
 
 const Skills = ({ id }) => {
   return (
     <Section height={700} id={id} >
 		  <Image alt='Background' src={bgimg} priority layout='fill' objectFit='cover' objectPosition='center'/>
       <Container>
-          <Heading inverse >Expertise</Heading>
+          <Heading>Expertise</Heading>
           <SkillsGrid>
               {skillData.map((skill, index) => (
-              <SkillWrapper key={index}>
+                <Tilt key={index} className="Tilt" options={{ max: 30, scale: 1, reset: false }}>
+                  <SkillWrapper>
                   <Image src={skill.img} width={64} height={64} layout='fixed'/>
                   <SkillText>{skill.text}</SkillText>
-              </SkillWrapper>
+                </SkillWrapper>
+                </Tilt>
+
+              
           ))}
           </SkillsGrid>
         </Container>
