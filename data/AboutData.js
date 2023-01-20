ef775a112d1527fa57d7126167e3624c5d618045
Maksym Sagadin <maxsagadin@gmail.com>
2023-01-19 18:49:29 -0800

    updated about me information

--- data/AboutData.js
+++ data/AboutData.js
@@ -1,8 +1,8 @@
 export const aboutData = {
 	headline: 'Who I Am',
-	intro: 'Working as an Application Administrator exposed me to HTML and inspired me to learn more about code and programming so I enrolled into De Anza College to study computer programming. There, I studied C++, Python and Data Structures. In one of my classes, we used a graphic user interface in our coding assignment and that was my initial spark for front end development.',
+	intro: 'Working as an Application Administrator exposed me to HTML and inspired me to learn more about code and programming so I enrolled into De Anza College to study software programming. There, I studied C++, Python and Data Structures.',
 	description:
-		'After realizing my passion for development with UI, I completed a web development bootcamp called Freemote. At Freemote, I learned HTML, CSS, Javascript, React, as well as building Responsive layouts, Mobile First design and e-commerce development with Shopify. Now I am looking for opportunities in front end web development to grow my career and skills.',
+		'In 2020, I had just finished at De Anza College, right before I lost my job and everyone got quarantined at home due to Covid. During quarantine I spent my time learning HTML, CSS, Javascript and React at Freemote. After completing Freemote, I completed freelance projects for local businesses and I am now looking for a full-time Front End Developer position.',
 	bgImg: '/backgrounds/bg-dark.png',
 	heroImg: '/hero_image.jpg',
 }
