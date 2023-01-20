d0b11634783cb1efa27a4e636c2857aca49d554c
Maksym Sagadin <maxsagadin@gmail.com>
2023-01-19 19:06:49 -0800

    updated speed property for Tilt and added transition property

--- components/Skills/Skills.js
+++ components/Skills/Skills.js
@@ -14,7 +14,7 @@ const Skills = ({ id }) => {
           <Heading>Expertise</Heading>
           <SkillsGrid>
             {skillData.map((skill, index) => (
-              <Tilt key={index} className="Tilt" options={{ max: 30, scale: 1, speed: 2000, reset: false }}>
+              <Tilt key={index} className="Tilt" options={{ max: 30, transition: true, scale: 1, speed: 3000, reset: false }}>
                 <SkillWrapper >
                   <Image src={skill.img} width={64} height={64} layout='fixed'/>
                   <SkillText>{skill.text}</SkillText>
