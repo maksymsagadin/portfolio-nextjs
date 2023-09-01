98925535b52157164d4e0016087f9ed3580049db
Maksym Sagadin <maxsagadin@gmail.com>
2023-09-01 16:27:52 -0700

    updated background to be smaller size

--- data/AboutData.js
+++ data/AboutData.js
@@ -3,6 +3,6 @@ export const aboutData = {
 	intro: 'As an Application Administrator, I was intrigued by HTML and motivated to delve deeper into the world of coding and programming. I enrolled at De Anza College to study software programming, where I honed my skills in C++, Python, and Data Structures.',
 	description:
 		'The COVID-19 pandemic in 2020 resulted in a shift in my career path, as I found myself out of work and with ample time to learn HTML, CSS, Javascript, and React through Freemote. This newfound knowledge led me to take on freelance projects for local businesses and solidified my passion for front-end development. I am now seeking a full-time position as a Front End Developer, eager to apply my skills and continue growing in this exciting field.',
-	bgImg: '/backgrounds/bg-dark.png',
+	bgImg: '/backgrounds/bg-dark.jpeg',
 	heroImg: '/hero-image.jpeg',
 }
